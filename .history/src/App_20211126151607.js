c8af649d7b9d585367b5d21dab889f54c9ec7cce
omar-slf <nurullayevumarali1@gmail.com>
2021-11-26 15:23:12 +0500

    no commit

---
+++ .history/src/App_20211126151607.js
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react";
+import Player from "./components/Player";
+
+function App() {
+  const artis = 'Morgenshtern'
+  const image = './images/morgen.jpg'
+  const [songs] = useState([
+    {
+      title: "Show",
+      artist: artis,
+      img_src: image,
+      src: "./music/SHOW.mp3",
+    },
+    {
+      title: "Dulo",
+      artist: "Morgenshtern",
+      img_src: "./images/nach-meri-rani.jpg",
+      src: "./music/DULO.mp3",
+    },
+    {
+      title: "Care Ni Karda",
+      artist: "Yo Yo Honey Singh",
+      img_src: "./images/care-ni-karda.jpg",
+      src: "./music/MORGENSHTERN_&_Тимати_El_Problema_Prod_SLAVA_MARLOW_Премьера_Клипа.mp3",
+    },
+    {
+      title: "Burj Khalifa",
+      artist: "Shashi",
+      img_src: "./images/burjkalifa.jpg",
+      src: "./music/BurjKhalifa.mp3",
+    },
+    {
+      title: "Tango Del Fuego",
+      artist: "Parov Stelar",
+      img_src: "./images/ParovStelarGeorgiaGibbs-TangoDelFuego.jpg",
+      src: "./music/ParovStelarGeorgiaGibbs-TangoDelFuego.mp3",
+    },
+    {
+      title: "Take You Dancing",
+      artist: "Jason Derulo",
+      img_src: "./images/JasonDerulo-TakeYouDancing.jpg",
+      src: "./music/JasonDerulo-TakeYouDancing.mp3",
+    },
+    {
+      title: "Daisy",
+      artist: "Ashnikko",
+      img_src: "./images/Ashnikko-Daisy.jpg",
+      src: "./music/Ashnikko-Daisy.mp3",
+    },
+    {
+      title: "Dolly Song",
+      artist: "Partz Grimbad",
+      img_src: "./images/PatzGrimbard-DollySong.jpg",
+      src: "./music/PatzGrimbard-DollySong.mp3",
+    },
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex, songs.length]);
+
+  return (
+    <div className="App">
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/src/App_20211126152153.js
@@ -0,0 +1,68 @@
+import { faImages } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
+import Player from "./components/Player";
+
+function App() {
+  const artis = 'Morgenshtern'
+  const image = './images/morgen.jpg'
+  const [songs] = useState([
+    {
+      title: "Show",
+      artist: artis,
+      img_src: image,
+      src: "./music/SHOW.mp3",
+    },
+    {
+      title: "Dulo",
+      artist: "Morgenshtern",
+      img_src: image,
+      src: "./music/DULO.mp3",
+    },
+    {
+      title: "El Problema",
+      artist: artis,
+      img_src: image,
+      src: "./music/MORGENSHTERN_&_Тимати_El_Problema_Prod_SLAVA_MARLOW_Премьера_Клипа.mp3",
+    },
+    {
+      title: 'Aristocrat',
+      artist: artis,
+      img_src: image,
+      src: "./music/ARISTOCRAT.mp3",
+    },
+ 
+    {
+      title: "Take You Dancing",
+      artist: "Jason Derulo",
+      img_src: "./images/JasonDerulo-TakeYouDancing.jpg",
+      src: "./music/JasonDerulo-TakeYouDancing.mp3",
+    },
+
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex, songs.length]);
+
+  return (
+    <div className="App">
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
+    </div>
+  );
+}
+
+export default App;
